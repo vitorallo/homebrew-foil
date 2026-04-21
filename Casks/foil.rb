751a4b9bfc27da7cb69faecd82c4d837f88ab5cd
Vito Rallo <vito.rallo@gmail.com>
2026-04-21 15:05:29 +0200

    v0.2.4-beta: language helper fix

--- Casks/foil.rb
+++ Casks/foil.rb
@@ -1,6 +1,6 @@
 cask "foil" do
   version "0.2.4-beta"
-  sha256 "d1e8669aad04c3a6f95a0ca4f40c61d35c737d22f6de8de6261fcd48e711ec2a"
+  sha256 "f3930a0b4440bf6bedd3dc5ef285fd9fd274a8e17dc9b20079d9681d56433201"
 
   url "https://github.com/vitorallo/foil-releases/releases/download/v#{version}/Foil.dmg"
   name "Foil"
